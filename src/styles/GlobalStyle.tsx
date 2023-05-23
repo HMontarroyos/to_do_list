74580f5f0253b2b7623ab74ce9d563bf588404a1
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-05-23 14:15:43 -0300

    feat: Add Google Fonts and implement global styling to the project

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components'
+export const GlobalStyle = createGlobalStyle`
+  * {
+    border: 0;
+    padding: 0;
+    margin: 0;
+    font-family: 'Oswald', sans-serif;
+  }
+
+  body{
+    background-color:  #F2E9e6;
+  }
+`
