456f447fb41dbba8f8998f4956af93ceee91c921
Hebert Montarroyos <hebertmontarroyos@hotmail.com>
2023-05-25 13:57:24 -0300

    style: Add new styles to the About page~

--- src/views/About/index.tsx
+++ src/views/About/index.tsx
@@ -1,41 +1,44 @@
-import React from 'react'
-import { Fade } from 'react-reveal'
-import * as S from './styled'
-import {toDoList} from "../../assets/images"
+import React from "react";
+import { Fade } from "react-reveal";
+import * as S from "./styled";
+import { toDoList } from "../../assets/images";
 
 const About: React.FC = () => {
   return (
-        <>
-            <S.Container>
-                <Fade top cascade>
-                    <div>
-                        <h1>Sobre Nós</h1>
-                        <p>
-                            Bem-vindo(a) ao nosso sistema de gerenciamento de tarefas! Sabemos o quão essencial é ser 
-                            organizado(a) e eficiente em suas atividades diárias. 
-                            Para ajudá-lo(a) a alcançar esse objetivo, criamos um recurso poderoso que permite 
-                            que você crie, personalize e acompanhe suas listas de tarefas com facilidade.
-                        </p>
-                        <p>
-                            Além disso, nosso sistema permite que você edite e gerencie suas tarefas, 
-                            adicionando sua lista de itens e caso deseje também pode estar criando 
-                            sub-itens relacionado aquela determinado tarefa. 
-                            E se alguma tarefa não for mais relevante, você pode excluí-la sem problemas.
-                        </p>
-                        <p>
-                            Então, não espere mais! começe logo a criar sua primeira lista de tarefas agora mesmo. 
-                            Com recursos abrangentes e uma interface intuitiva, estamos aqui para ajudá-lo(a) a 
-                            atingir níveis elevados de produtividade e alcançar seus objetivos. Seja organizado(a), 
-                            seja eficiente e conquiste o sucesso em suas tarefas diárias!
-                        </p>             
-                    </div>
-                </Fade>
-                <Fade right>
-                    <S.Image src={toDoList}/>
-                </Fade>
-            </S.Container>
-        </>
-)
-}
+    <>
+      <S.Container>
+        <Fade top cascade>
+          <div>
+            <h1>Sobre Nós</h1>
+            <p>
+              Bem-vindo(a) ao nosso sistema de gerenciamento de tarefas! Sabemos
+              o quão essencial é ser organizado(a) e eficiente em suas
+              atividades diárias. Para ajudá-lo(a) a alcançar esse objetivo,
+              criamos um recurso poderoso que permite que você crie, personalize
+              e acompanhe suas listas de tarefas com facilidade.
+            </p>
+            <p>
+              Além disso, nosso sistema permite que você edite e gerencie suas
+              tarefas, adicionando sua lista de itens e caso deseje também pode
+              estar criando sub-itens relacionado aquela determinado tarefa. E
+              se alguma tarefa não for mais relevante, você pode excluí-la sem
+              problemas.
+            </p>
+            <p>
+              Então, não espere mais! começe logo a criar sua primeira lista de
+              tarefas agora mesmo. Com recursos abrangentes e uma interface
+              intuitiva, estamos aqui para ajudá-lo(a) a atingir níveis elevados
+              de produtividade e alcançar seus objetivos. Seja organizado(a),
+              seja eficiente e conquiste o sucesso em suas tarefas diárias!
+            </p>
+          </div>
+        </Fade>
+        <Fade right>
+          <S.Image src={toDoList} />
+        </Fade>
+      </S.Container>
+    </>
+  );
+};
 
-export default About
+export default About;
--- src/views/About/styled.tsx
+++ src/views/About/styled.tsx
@@ -1,28 +1,27 @@
-import styled from 'styled-components'
+import styled from "styled-components";
 
 export const Container = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    width: 800px;
-    margin: 20px auto;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 800px;
+  margin: 20px auto;
 
-    h1 {
-        color: ${(props) => props.theme.colors.text};
-        font-size: ${(props) => props.theme.fontSizes.large};
-        font-family: ${(props) => props.theme.fonts.title};
-        margin-top: 20px;
-    }
-    p {
-        color: ${(props) => props.theme.colors.text};
-        font-size: ${(props) => props.theme.fontSizes.small};
-        font-family: ${(props) => props.theme.fonts.title};
-        margin-top: 20px;
-    }
-`
+  h1 {
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.large};
+    font-family: ${(props) => props.theme.fonts.title};
+    margin-top: 20px;
+  }
+  p {
+    color: ${(props) => props.theme.colors.text};
+    font-size: ${(props) => props.theme.fontSizes.small};
+    font-family: ${(props) => props.theme.fonts.title};
+    margin-top: 20px;
+  }
+`;
 export const Image = styled.img`
-width: 400px;
-height: 400px;
-
-`
+  width: 400px;
+  height: 400px;
+`;
